b83c2cad4007ce9b3b033a5fefc21035668ef53d
Mkejji Phone <mouradif@gmail.com>
2016-10-05 16:21:17 +0200

    parse map

--- src/parse.c
+++ src/parse.c
@@ -11,3 +11,30 @@ t_game	*init(void)
 
 	g = (t_game*)malloc(sizeof(t_game));
 }
+
+int		read_map(t_game *g)
+{
+
+	int		i;
+	int		j;
+char	*line;
+	int		len;
+
+	j = 0;
+	while((len = ft_get_next_line(1, &line)) > 0 && j < g->height)
+	{
+		i = 0;
+		while (line[i] && line[i] != ' ')
+			i++;
+		while(line[i] && i < g->map->width)
+		{
+			g->map->grid[j][i] = (char) ft_toupper(line[i]);
+			i++;
+		}
+		if (i != g->map->width)
+			return (ft_freedie(&g, "Bad map info"));
+		j++;
+	}
+	if (j != g->map->height)
+		return (ft_freedie(&g, "Bad map info"));
+}
---
+++ src/parseutils.c
@@ -0,0 +1,13 @@
+#include "filler.h"
+#include "libft.h"
+#include <stdlib.h>
+
+int	get_player_info(t_game* g, char *line)
+{
+	return (1);
+}
+
+int get_map_info(t_game* g, char *line)
+{
+
+}
